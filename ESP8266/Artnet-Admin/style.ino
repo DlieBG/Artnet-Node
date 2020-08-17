a9ad08809f501044914fd2a69aa53620060e4ba1
Benedikt <mail@benedikt-schwering.de>
2020-08-17 11:31:01 +0200

    html

--- ESP8266/Artnet-Admin/style.ino
+++ ESP8266/Artnet-Admin/style.ino
@@ -1,4 +1,4 @@
 String style()
 {
-  return "
+  return "body { font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; } h1 { margin: 10px; } h4 { margin-top: 5px; margin-bottom: 10px; } .card { border: 1px solid black; border-radius: 25px; } .form-control { width: 50%; margin-bottom: 2px; }";
 }
